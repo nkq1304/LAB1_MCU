ed2865afd2912661f479b6a8843478b1a537c376
kimquynh <quynh.nguyenkim1304@hcmut.edu.vn>
2023-10-12 16:01:07 +0700

    delete clearNumberOnClock(int num) and add clearAllClock()

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -92,43 +92,19 @@ void turnOnLedSeq(int num){
 		HAL_GPIO_TogglePin(LED11_GPIO_Port, LED11_Pin);
 	}
 }
-void clearNumberOnClock(int num){
-	if(num==0){
-		HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, SET);
-	}
-	if(num==1){
-		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, SET);
-	}
-	if(num==2){
-		HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, SET);
-	}
-	if(num==3){
-		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, SET);
-	}
-	if(num==4){
-		HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, SET);
-	}
-	if(num==5){
-		HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, SET);
-	}
-	if(num==6){
-		HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, SET);
-	}
-	if(num==7){
-		HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, SET);
-	}
-	if(num==8){
-		HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, SET);
-	}
-	if(num==9){
-		HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, SET);
-	}
-	if(num==10){
-		HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, SET);
-	}
-	if(num==11){
-		HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, SET);
-	}
+void clearAllClock () {
+	HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, SET);
+	HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, SET);
+	HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, SET);
+	HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, SET);
+	HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, SET);
+	HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, SET);
+	HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, SET);
+	HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, SET);
+	HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, SET);
+	HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, SET);
+	HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, SET);
+	HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, SET);
 }
 /* USER CODE END 0 */
 

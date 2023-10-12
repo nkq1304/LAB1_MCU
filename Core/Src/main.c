714657d2be927249277eccf3464a77b069d0aa4a
kimquynh <quynh.nguyenkim1304@hcmut.edu.vn>
2023-10-12 15:34:47 +0700

    add ex6 v0

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -47,13 +47,51 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+void turnOnLedSeq(int num){
+	if(num==0){
+		HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+	}
+	if(num==1){
+		HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+	}
+	if(num==2){
+		HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
+	}
+	if(num==3){
+		HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
+	}
+	if(num==4){
+		HAL_GPIO_TogglePin(LED4_GPIO_Port, LED4_Pin);
+	}
+	if(num==5){
+		HAL_GPIO_TogglePin(LED5_GPIO_Port, LED5_Pin);
+	}
+	if(num==6){
+		HAL_GPIO_TogglePin(LED6_GPIO_Port, LED6_Pin);
+	}
+	if(num==7){
+		HAL_GPIO_TogglePin(LED7_GPIO_Port, LED7_Pin);
+	}
+	if(num==8){
+		HAL_GPIO_TogglePin(LED8_GPIO_Port, LED8_Pin);
+	}
+	if(num==9){
+		HAL_GPIO_TogglePin(LED9_GPIO_Port, LED9_Pin);
+	}
+	if(num==10){
+		HAL_GPIO_TogglePin(LED10_GPIO_Port, LED10_Pin);
+	}
+	if(num==11){
+		HAL_GPIO_TogglePin(LED11_GPIO_Port, LED11_Pin);
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -83,16 +121,23 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  int num =0;
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  turnOnLedSeq(num);
+	  num++;
+	  if(num>=12){
+		  num = 0;
+	  }
+	  HAL_Delay(1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -131,6 +176,36 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED0_Pin|LED1_Pin|LED2_Pin|LED3_Pin
+                          |LED4_Pin|LED5_Pin|LED6_Pin|LED7_Pin
+                          |LED8_Pin|LED9_Pin|LED10_Pin|LED11_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED0_Pin LED1_Pin LED2_Pin LED3_Pin
+                           LED4_Pin LED5_Pin LED6_Pin LED7_Pin
+                           LED8_Pin LED9_Pin LED10_Pin LED11_Pin */
+  GPIO_InitStruct.Pin = LED0_Pin|LED1_Pin|LED2_Pin|LED3_Pin
+                          |LED4_Pin|LED5_Pin|LED6_Pin|LED7_Pin
+                          |LED8_Pin|LED9_Pin|LED10_Pin|LED11_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LAB1_MCU
+The 'Branch EX6' contains the STM32 source code and a Proteus folder in exercise 6.
